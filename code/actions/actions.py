44f72c01af098e13e82a9c647a7ba1633314b055
Tiago Sora <tiagogcarvalho@ua.pt>
2023-11-08 19:37:46 +0000

    creio estar 90%

--- code/actions/actions.py
+++ code/actions/actions.py
@@ -45,27 +45,6 @@ def write_log(text):
     with open("log.txt", "a") as log:
         log.write(text)
 
-def get_credentials(self) -> Credentials:
-    """Gets valid user credentials from storage."""
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    return creds
-
 class ActionDefaultFallback(Action):
     """Executes the fallback action and goes back to the previous state
     of the dialogue"""
@@ -94,69 +73,66 @@ class ActionDefaultFallback(Action):
         # Revert user message which led to fallback.
         return [UserUtteranceReverted()]
     
-class ActionListAllEvents(Action):
+# class ActionListAllEvents(Action):
 
-    def name(self) -> Text:
-        return "action_list_all_events"
+#     def name(self) -> Text:
+#         return "action_list_all_events"
 
-    async def run(
-        self, 
-        dispatcher: CollectingDispatcher, 
-        tracker: Tracker, 
-        domain: Dict[Text, Any]
-    ) -> List[Dict[Text, Any]]:
+#     async def run(
+#         self, 
+#         dispatcher: CollectingDispatcher, 
+#         tracker: Tracker, 
+#         domain: Dict[Text, Any]
+#     ) -> List[Dict[Text, Any]]:
         
-        dispatcher.utter_message("Bilhaaaaaaaaaa")
+#         dispatcher.utter_message("Bilhaaaaaaaaaa")
 
-        if tracker.latest_message["intent"].get("confidence") < 0.8:
-            dispatcher.utter_message(response="utter_default")
+#         if tracker.latest_message["intent"].get("confidence") < 0.8:
+#             dispatcher.utter_message(response="utter_default")
 
-        creds = get_credentials()
-        service = build('calendar', 'v3', credentials=creds)
+#         creds = get_credentials()
+#         service = build('calendar', 'v3', credentials=creds)
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
+#         # Call the Calendar API
+#         now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+#         print('Getting the upcoming 10 events')
+#         events_result = service.events().list(calendarId='primary', timeMin=now,
+#                                               maxResults=10, singleEvents=True,
+#                                               orderBy='startTime').execute()
+#         events = events_result.get('items', [])
 
-        if not events:
-            dispatcher.utter_message(text="No upcoming events found.")
-        else:
-            # Format the events into a response
-            message = "Here are your upcoming events:"
-            for event in events:
-                start = event['start'].get('dateTime', event['start'].get('date'))
-                message += f"\n- {event['summary']} at {start}"
-            dispatcher.utter_message(text=message)
+#         if not events:
+#             dispatcher.utter_message(text="No upcoming events found.")
+#         else:
+#             # Format the events into a response
+#             message = "Here are your upcoming events:"
+#             for event in events:
+#                 start = event['start'].get('dateTime', event['start'].get('date'))
+#                 message += f"\n- {event['summary']} at {start}"
+#             dispatcher.utter_message(text=message)
+#        return []
 
-        return []
-
-class ActionListAllEventsOfADate(Action):
+# class ActionListAllEventsOfADate(Action):
     
-    def name(self) -> Text:
-        return "action_list_all_events_of_a_date"
+#     def name(self) -> Text:
+#         return "action_list_all_events_of_a_date"
     
-    async def run(
-            self, 
-            dispatcher: CollectingDispatcher, 
-            tracker: Tracker, 
-            domain: Dict[Text, Any]
-        ) -> List[Dict[Text, Any]]:
+#     async def run(
+#             self, 
+#             dispatcher: CollectingDispatcher, 
+#             tracker: Tracker, 
+#             domain: Dict[Text, Any]
+#         ) -> List[Dict[Text, Any]]:
 
-        if tracker.latest_message["intent"].get("confidence") < 0.8:
-            dispatcher.utter_message(response="utter_default")
+#         if tracker.latest_message["intent"].get("confidence") < 0.8:
+#             dispatcher.utter_message(response="utter_default")
 
-        day = tracker.get_slot('day')
-        month = tracker.get_slot('month')
-        year = tracker.get_slot('year')
+#         day = tracker.get_slot('day')
+#         month = tracker.get_slot('month')
+#         year = tracker.get_slot('year')
 
-        events_list = None # TODO FAZER A SAUCE :)
+#         events_list = None # TODO FAZER A SAUCE :)
 
-        dispatcher.utter_message(response="utter_confirm_date", day=day, month=month, year=year)
-        dispatcher.utter_message(response="utter_events_listed", events=events_list)
 
 class ActionAfirmar(Action):
     
